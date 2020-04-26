19a97de8ed6e3cee8bb882577ae9aaef7871d4b9
Braxton <Braxtonmassengale@outlook.com>
2020-04-26 13:06:37 +0200

    Works with 1 word arguments

--- main.rb
+++ main.rb
@@ -1,3 +1,20 @@
+test_array = %w[win winning winner how howdy howard]
+
 def substrings(word, arr)
+  result = Hash.new
+  word_count = 0
+  result[word] = word_count
   
+  arr.each do
+  |elem|
+    if
+    elem.include?(word)
+      word_count +=1
+    end
+    result[word] = word_count
+  end
+
+  puts result
 end
+
+substrings("win", test_array)
